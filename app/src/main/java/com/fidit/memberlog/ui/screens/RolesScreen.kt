791d912e293e6f366f9e9514303a314915d8fda1
Emma Reljić <emma.reljic1@gmail.com>
2026-06-24 15:05:49 +0200

    Optimizacija koda

--- app/src/main/java/com/fidit/memberlog/ui/screens/RolesScreen.kt
+++ app/src/main/java/com/fidit/memberlog/ui/screens/RolesScreen.kt
@@ -1,7 +1,6 @@
 package com.fidit.memberlog.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,13 +18,10 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
